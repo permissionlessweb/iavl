32abd8b7ea09fee26c62243f821eba8eadd29528
hard-nett <hardnettt@proton.me>
2026-01-20 18:30:08 -0500

    benchmarks

--- benchmarks/hash_test.go
+++ benchmarks/hash_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/cosmos/iavl"
+	iavlhash "github.com/cosmos/iavl/hash"
 	"github.com/stretchr/testify/require"
 
 	_ "crypto/sha256"
@@ -17,6 +18,8 @@ import (
 
 func BenchmarkHash(b *testing.B) {
 	fmt.Printf("%s\n", iavl.GetVersionInfo())
+
+	// Standard hash.Hash implementations
 	hashers := []struct {
 		name string
 		size int
@@ -37,6 +40,26 @@ func BenchmarkHash(b *testing.B) {
 			benchHasher(sub, hasher.hash, hasher.size)
 		})
 	}
+
+	// IAVL hash.Hasher implementations
+	iavlHashers := []struct {
+		name   string
+		size   int
+		hasher iavlhash.Hasher
+	}{
+		{"iavl-sha256", 64, iavlhash.NewSHA256Hasher()},
+		{"iavl-sha256", 512, iavlhash.NewSHA256Hasher()},
+		{"iavl-poseidon", 64, iavlhash.NewPoseidonHasher()},
+		{"iavl-poseidon", 512, iavlhash.NewPoseidonHasher()},
+	}
+
+	for _, h := range iavlHashers {
+		prefix := fmt.Sprintf("%s-%d", h.name, h.size)
+		hasher := h
+		b.Run(prefix, func(sub *testing.B) {
+			benchIAVLHasher(sub, hasher.hasher, hasher.size)
+		})
+	}
 }
 
 func benchHasher(b *testing.B, hash hash.Hash, size int) {
@@ -49,5 +72,16 @@ func benchHasher(b *testing.B, hash hash.Hash, size int) {
 		_, err := hash.Write(inputs[i : i+size])
 		require.NoError(b, err)
 		hash.Sum(nil)
+	}
+}
+
+func benchIAVLHasher(b *testing.B, hasher iavlhash.Hasher, size int) {
+	// create all random bytes before to avoid timing this
+	inputs := randBytes(b.N + size + 1)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		// grab a slice of size bytes from random string
+		hasher.HashValue(inputs[i : i+size])
 	}
 }
